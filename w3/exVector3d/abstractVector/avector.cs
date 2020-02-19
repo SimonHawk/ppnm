11806f3961496e9a32443cc34f98fdb3af5d6a59
Simon Høgh Albrechtsen <simonhogh@gmail.com>
2020-02-19 15:20:16 +0100

    Fixed up avector, which now works

--- w3/exVector3d/abstractVector/avector.cs
+++ w3/exVector3d/abstractVector/avector.cs
@@ -3,8 +3,40 @@ public abstract class avector {
 	public abstract double y{get; set;}	
 	public abstract double z{get; set;}
 
-	public static avector operator+(avector v, avector u) {
-		return _add(v, u);
+	public avector add(avector other) {
+		this.x = this.x + other.x;
+		this.y = this.y + other.y;
+		this.z = this.z + other.z;
+		return this;
+	}
+
+	public avector sub(avector other) {
+		this.x = this.x - other.x;
+		this.y = this.y - other.y;
+		this.z = this.z - other.z;
+		return this;
+	}
+
+	public avector mult(double a) {
+		this.x = this.x*a;
+		this.y = this.y*a;
+		this.z = this.z*a;
+		return this;
+	}	
+
+	public double dot_product(avector other) {
+		return this.x*other.x + this.y*other.y + this.z*other.z;
+	} 
+
+	public avector cross_product(avector other) {
+		this.x = this.y*other.z - this.z*other.y;
+		this.y = this.z*other.x - this.x*other.y;
+		this.z = this.x*other.y - this.y*other.x;
+		return this;
+	}
+	
+	public double magnitude() {
+		return System.Math.Sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
 	}
 
 	public override string ToString() {
--- w3/exVector3d/abstractVector/main.cs
+++ w3/exVector3d/abstractVector/main.cs
@@ -7,7 +7,13 @@ class main {
 		Write("v = {0}\n", v);
 		Write("v2 = {0}\n", v2);
 
-		Write("v + v2 = {0}\n", v + v2);
+		Write("v + v2 = {0}\n", v.add(v2));
+		
+		Write("v dot v2 = {0}\n", v.dot_product(v2));
+		
+		Write("v cross v2 = {0}\n", v.cross_product(v2));
+		
+		Write("|v| = {0}\n", v.magnitude());
 		
 	}
 }
