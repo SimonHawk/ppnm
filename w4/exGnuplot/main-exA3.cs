92e9e8f79b71aeeff586315c6941d9d262f4762f
Simon Høgh Albrechtsen <simonhogh@gmail.com>
2020-02-21 17:01:59 +0100

    Made Exercise B in Gnuplot

---
+++ w4/exGnuplot/main-exA3.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+
+class main {
+	static void Main() {
+		double deltax = 1.0/16;
+		double eps = 1.0/64;
+		for(double x = -5+eps; x<=5; x += deltax) {
+			Write("{0,15:f8} {1,15:f8}\n", x, math.gamma(x));			
+		}
+	}
+}
--- w4/lecGNUPlot/main-gamma.cs
+++ w4/lecGNUPlot/main-gamma.cs
@@ -1,4 +1,5 @@
 using static System.Console;
+using static System.Math;
 
 class main {
 	static int Main() {
@@ -7,6 +8,12 @@ class main {
 		for(double x=-4+eps; x <= 6; x+=dx) {
 			WriteLine("{0,10:f3} {1,15:f8}", x, math.gamma(x));
 		}	
+		double tg = 1;
+		for(double x = 1; x <= 170; x+=1) {
+			var gx = math.gamma(x);
+			WriteLine("{0,10:f3} {1,15:f8}", x, Abs(gx-tg)/tg);
+			tg = tg*x;
+		}
 		return 0;		
 
 	} // Main
