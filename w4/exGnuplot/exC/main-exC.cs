fbe36968976d7b548e3a933c913e7e42b22ef3d9
Simon Høgh Albrechtsen <simonhogh@gmail.com>
2020-02-24 16:28:43 +0100

    3d plot of gamma function works, but plot could be nicer

--- w4/exGnuplot/exC/main-exC.cs
+++ w4/exGnuplot/exC/main-exC.cs
@@ -11,9 +11,11 @@ class main {
 		for(double re = min+eps; re<=max; re += delta) {
 			for(double im = min+eps; im<=max; im += delta) {
 				z = new complex(re, im);
-				Write("{0:f8} {1:f8} {2:f8}\n", re, im, math.absgamma(z));
+				// Start of including the phase in the plot.
+				complex gammaValue = math.gamma(z);
+				Write("{0:f8} {1:f8} {2:f8} {3:f8}\n", re, im, abs(gammaValue), complex.argument(gammaValue));
 			}
-			Write("\n\n\n");
+			Write("\n");
 		}
 	}
 }
