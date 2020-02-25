aecad2a838c24b51a2a1c6176e3225cf5771a06c
Simon Høgh Albrechtsen <simonhogh@gmail.com>
2020-02-25 11:44:23 +0100

    Made smarter sampling for ExB for prettier plot

--- w4/exGnuplot/main-exB.cs
+++ w4/exGnuplot/main-exB.cs
@@ -2,10 +2,11 @@ using static System.Console;
 
 class main {
 	static void Main() {
-		double deltax = 1.0/512;
-		double eps = 1.0/1024;
-		for(double x = -5+eps; x<=5; x += deltax) {
-			Write("{0,15:f8} {1,15:f8}\n", x, math.lngamma(x));			
+		double deltax = 1.0/32;
+		double eps = 1.0/(1024*4);
+		for(double x = -5; x<=5; x += deltax) {
+			Write("{0,15:f8} {1,15:f8}\n", x-eps, math.lngamma(x-eps));			
+			Write("{0,15:f8} {1,15:f8}\n", x+eps, math.lngamma(x+eps));			
 		}
 	}
 }
