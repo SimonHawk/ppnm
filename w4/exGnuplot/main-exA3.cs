b4db7a7db6827c2b0c7f2a2240a8e3d0726ff8c5
Simon Høgh Albrechtsen <simonhogh@gmail.com>
2020-02-25 11:28:00 +0100

    Fixed exercise A3 by adding assemply reference cmath.dll to main-eexa3.cs compilation

--- w4/exGnuplot/main-exA3.cs
+++ w4/exGnuplot/main-exA3.cs
@@ -2,10 +2,11 @@ using static System.Console;
 
 class main {
 	static void Main() {
-		double deltax = 1.0/16;
-		double eps = 1.0/64;
-		for(double x = -5+eps; x<=5; x += deltax) {
-			Write("{0,15:f8} {1,15:f8}\n", x, math.gamma(x));			
+		double deltax = 1.0/32;
+		double eps = 1.0/512;
+		for(double x = -5; x<=5; x += deltax) {
+			Write("{0,15:f8} {1,15:f8}\n", x-eps, math.gamma(x-eps));			
+			Write("{0,15:f8} {1,15:f8}\n", x+eps, math.gamma(x+eps));			
 		}
 	}
 }
